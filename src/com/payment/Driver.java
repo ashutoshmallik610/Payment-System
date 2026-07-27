6ca74cc095bd2d153d46cdd6fd793ceda53ecb6c
ashutoshmallik610 <157103768+ashutoshmallik610@users.noreply.github.com>
2026-07-27 18:20:36 +0530

    Driver

--- src/com/payment/Driver.java
+++ src/com/payment/Driver.java
@@ -10,7 +10,7 @@ public class Driver {
 		
 		System.out.println("Choose Payment Type");
         System.out.println("1. UPI.");
-        System.out.println("2. CreditCard");
+        System.out.println("2. CreditCard.");
         System.out.println("3. Netbanking");
         int choice=sc.nextInt();
 		
